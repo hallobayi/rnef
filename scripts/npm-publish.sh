dab7d58ed9a9491dcab0baf2ac821992eb5c282d
Michał Pierzchała <thymikee@gmail.com>
2026-04-08 16:57:43 +0200

    [codex] Enable npm trusted publishing (#698)
    * chore: enable npm trusted publishing
    
    * chore: bump GitHub Actions pins to v6
    
    * chore: bump template action pins to v6

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -5,13 +5,13 @@ echo "Building all packages..."
 
 pnpm build
 
-if [ -z "$NPM_TOKEN" ]; then
+if [ -z "$NPM_TOKEN" ] && [ -z "$CI" ] && [ -z "$GITHUB_ACTIONS" ]; then
   read -p "Enter NPM OTP: " OTP
 fi
 
 echo "NPM: Publishing all packages"
-# If NPM_TOKEN is set (CI environment), use it
-if [ -n "$NPM_TOKEN" ]; then
+# In CI, trusted publishing should be non-interactive and not require OTP input.
+if [ -n "$NPM_TOKEN" ] || [ -n "$CI" ] || [ -n "$GITHUB_ACTIONS" ]; then
   pnpm -r run publish:npm
 else
   pnpm -r --no-bail run publish:npm --otp="$OTP"
@@ -19,8 +19,8 @@ fi
 
 echo "NPM: Publishing template"
 cd templates/rock-template-default
-# If NPM_TOKEN is set (CI environment), use it
-if [ -n "$NPM_TOKEN" ]; then
+# In CI, trusted publishing should be non-interactive and not require OTP input.
+if [ -n "$NPM_TOKEN" ] || [ -n "$CI" ] || [ -n "$GITHUB_ACTIONS" ]; then
   npm publish
 else
   npm publish --otp="$OTP"
